c2b184d3c2344304bb6d26bf7ac13c57d1a7f5aa
Leonardo <leonardo.alcedo@saggee.cl>
2025-05-06 20:50:43 -0400

    agrego estrucutura y el modulo de users

---
+++ app/services/users/create.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+const operations = require('../../config/db/operations');
+
+module.exports = class CreateService {
+
+    async process(req, res) {
+
+        try {
+            
+            console.log('CreateService - process - Creación de usuario');
+
+        let entry = {
+            user: req.body.user,
+            password: req.body.password,
+            name: req.body.name,
+            cedula: req.body.cedula,
+            email: req.body.email,
+            role: req.body.role,
+            state: req.body.state
+        };
+
+        if (!entry.user)
+            return res.status(400).json({ message: 'El usuario es requerido' });
+
+        if (!entry.password)
+            return res.status(400).json({ message: 'La contraseña es requerida' });
+
+        if (!entry.name)
+            return res.status(400).json({ message: 'El nombre es requerido' });
+
+        if (!entry.cedula)
+            return res.status(400).json({ message: 'La cedula es requerida' });
+
+        if (!entry.email)
+            return res.status(400).json({ message: 'El email es requerido' });
+
+        if (!entry.role)
+            return res.status(400).json({ message: 'El rol es requerido' });
+
+        if (!entry.state)
+            return res.status(400).json({ message: 'El estado es requerido' });
+
+        entry = {
+            user: entry.user.trim(),
+            password: entry.password.trim(),
+            name: entry.name.trim(),
+            cedula: entry. cedula.trim(),
+            email: entry.email.trim(),
+            role: entry.role.trim(),
+            state: entry.state.trim(),
+        };
+
+        const userWithCedula = await operations.findOne('users', { 'cedula': entry.cedula });
+        if (userWithCedula)
+            return res.status(400).json({ message: 'La cedula ya existe' });
+
+        const userWithEmail = await operations.findOne('users', { 'email': entry.email });
+        if (userWithEmail)
+            return res.status(400).json({ message: 'El email ya existe' });
+
+        const userWithUsername = await operations.findOne('users', { 'user': entry.user });
+        if (userWithUsername)
+            return res.status(400).json({ message: 'El usuario ya existe' });
+
+        const regEx = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>\/?]).{8,}$/;
+        if (!regEx.test(entry.password))
+            return res.status(400).json({ message: 'La contraseña debe tener al menos 8 caracteres, 1 dígito, 1 caracter en mayúscula y un caracter especial' });
+        const hashedPassword = await bcrypt.hash(entry.password, 10);
+
+        const newUser = {
+            user: entry.user,
+            name: entry.name,
+            cedula: entry.cedula,
+            email: entry.email,
+            role: entry.role,
+            password: hashedPassword,
+            state: entry.state,
+        };
+
+        await operations.insertOne('users', newUser);
+
+        return res.status(200).json({ message: 'Usuario creado exitosamente' });
+
+        } catch (error) {
+            console.error('CreateService - process - Error al crear el usuario', error);
+            throw error;
+        }
+    }
+}
---
+++ app/services/users/edit.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const { ObjectId } = require('mongodb');
+const bcrypt = require('bcrypt');
+const operations = require('../../config/db/operations');
+
+module.exports = class EditService {
+
+    async process(req, res) {
+
+        try {
+
+            console.log('EditService - process - Editar usuario');
+
+        const idUser = req.body._id;
+
+        const existingUser = await operations.findOne('users', { _id: ObjectId.createFromHexString(idUser) });
+        if (!existingUser)
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+
+        const { user, password, name, cedula, email, role, state } = req.body.user;
+        if (!user)
+            return res.status(400).json({ message: 'El usuario es requerido' });
+
+        if (!password)
+            return res.status(400).json({ message: 'La contraseña es requerida' });
+
+        if (!name)
+            return res.status(400).json({ message: 'El nombre es requerido' });
+
+        if (!cedula)
+            return res.status(400).json({ message: 'La cedula es requerida' });
+
+        if (!email)
+            return res.status(400).json({ message: 'El email es requerido' });
+
+        if (!role)
+            return res.status(400).json({ message: 'El rol es requerido' });
+
+        if (!state)
+            return res.status(400).json({ message: 'El estado es requerido' });
+
+        const regEx = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>\/?]).{8,}$/;
+        if (!regEx.test(password))
+            return res.status(400).json({ message: 'La contraseña debe tener al menos 8 caracteres, 1 dígito, 1 caracter en mayúscula y un caracter especial' });
+
+        if (email !== existingUser.email) {
+            const userWithEmail = await operations.findOne('users', { email });
+            if (userWithEmail)
+                return res.status(400).json({ message: 'El email ya existe' });
+        }
+
+        if (user !== existingUser.user) {
+            const userWithUsername = await operations.findOne('users', { user });
+            if (userWithUsername)
+                return res.status(400).json({ message: 'El usuario ya existe' });
+        }
+
+        if (cedula !== existingUser.cedula) {
+            const userWithCedula = await operations.findOne('users', { cedula });
+            if (userWithCedula)
+                return res.status(400).json({ message: 'La cedula ya existe' });
+        }
+
+        let hashedPassword = existingUser.password;
+        if (password !== existingUser.password)
+            hashedPassword = await bcrypt.hash(password, 10);
+
+        const updatedUser = {
+            $set: {
+                user,
+                password: hashedPassword,
+                name,
+                cedula,
+                email,
+                role,
+                state,
+            }
+        };
+
+        await operations.updateOne('users', { _id: ObjectId.createFromHexString(idUser) }, updatedUser);
+
+        return res.status(200).json({ message: 'Usuario actualizado exitosamente' });
+
+        } catch (error) {
+
+            console.error('EditService - process - Error al editar el usuario', error);
+            throw error;
+        }
+    }
+}
